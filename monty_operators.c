16c6ed77b820b3d3b5a46e5b4f223bba2671b89a
Molta1 <ayubamolta@gmail.com>
2022-12-15 10:11:12 -0800

    Stack Queues

--- monty_operators.c
+++ monty_operators.c
@@ -111,27 +111,27 @@ void f_div(stack_t **head, unsigned int counter)
 	h = *head;
 	while (h)
 	{
-OA		h = h->next;
-OA		length++;
+		h = h->next;
+		length++;
 	}
-OA	if (length < 2)
-OAOA	{
+	if (length < 2)
+	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-OA		fclose(bus.file);
-OAOA		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-OA	}
-	h = *head;
-	if (h->n == 0)
-OA	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-OAOA		fclose(bus.file);
+		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-OA	temp = h->next->n / h->n;
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = h->next->n / h->n;
 	h->next->n = temp;
 	*head = h->next;
 	free(h);
